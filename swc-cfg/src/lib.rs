89d2f331a76e6a79f0a5ad2999758f6819fab7da
Graham Kelly <gkgoat6700@gmail.com>
2024-10-24 22:02:28 -0400

    reloop

--- swc-cfg/src/lib.rs
+++ swc-cfg/src/lib.rs
@@ -1,27 +1,320 @@
-use std::{collections::HashMap, default};
+use std::{
+    collections::{BTreeSet, HashMap},
+    default,
+    iter::once,
+};
 
 use anyhow::Context;
 use id_arena::{Arena, Id};
+use relooper::ShapedBlock;
 use swc_atoms::Atom;
-use swc_common::{Spanned, SyntaxContext};
+use swc_common::{Span, Spanned, SyntaxContext};
 use swc_ecma_ast::{
-    BindingIdent, BlockStmt, Bool, Decl, DoWhileStmt, Expr, ExprStmt, Ident, IfStmt, Lit, Pat,
-    Stmt, WhileStmt,
+    ArrayLit, AssignExpr, BindingIdent, BlockStmt, Bool, BreakStmt, CallExpr, CatchClause,
+    ContinueStmt, Decl, DoWhileStmt, Expr, ExprOrSpread, ExprStmt, Ident, IdentName, IfStmt,
+    LabeledStmt, Lit, MemberExpr, Pat, ReturnStmt, Stmt, Str, SwitchCase, SwitchStmt, ThrowStmt,
+    TryStmt, WhileStmt,
 };
 
 pub struct Cfg {
     pub blocks: Arena<Block>,
+}
+impl Cfg {
+    pub fn reloop_block(&self, entry: Id<Block>) -> ShapedBlock<Id<Block>> {
+        return *relooper::reloop(
+            self.blocks
+                .iter()
+                .map(|(a, k)| {
+                    (
+                        a,
+                        match &k.catch {
+                            Catch::Throw => None,
+                            Catch::Jump { pat, k } => Some(*k),
+                        }
+                        .into_iter()
+                        .chain(
+                            match &k.term {
+                                Term::Return(expr) => vec![],
+                                Term::Throw(expr) => vec![],
+                                Term::Jmp(id) => vec![*id],
+                                Term::CondJmp {
+                                    cond,
+                                    if_true,
+                                    if_false,
+                                } => vec![*if_true, *if_false],
+                                Term::Switch { x, blocks, default } => {
+                                    blocks.values().cloned().chain(once(*default)).collect()
+                                }
+                                Term::Default => vec![],
+                            }
+                            .into_iter(),
+                        )
+                        .collect::<BTreeSet<_>>()
+                        .into_iter()
+                        .collect(),
+                    )
+                })
+                .collect(),
+            entry,
+        );
+    }
+    pub fn process_block(
+        &self,
+        k: &ShapedBlock<Id<Block>>,
+        span: Span,
+        ctxt: SyntaxContext,
+    ) -> Vec<Stmt> {
+        match k {
+            ShapedBlock::Simple(simple_block) => {
+                let jmp = |k: Id<Block>| {
+                    vec![Stmt::Expr(ExprStmt {
+                        span,
+                        expr: Box::new(Expr::Assign(AssignExpr {
+                            span,
+                            op: swc_ecma_ast::AssignOp::Assign,
+                            left: Ident::new(Atom::new("cff"), span, ctxt).into(),
+                            right: Box::new(Expr::Lit(Lit::Str(Str {
+                                span,
+                                value: Atom::new(k.index().to_string()),
+                                raw: None,
+                            }))),
+                        })),
+                    })]
+                    .into_iter()
+                    .chain(
+                        match simple_block.branches.get(&k) {
+                            None => vec![],
+                            Some(a) => match a {
+                                relooper::BranchMode::LoopBreak(l)
+                                | relooper::BranchMode::LoopBreakIntoMulti(l) => {
+                                    vec![Stmt::Break(BreakStmt {
+                                        span,
+                                        label: Some(Ident::new(
+                                            Atom::new(format!("${l}")),
+                                            span,
+                                            ctxt,
+                                        )),
+                                    })]
+                                }
+                                relooper::BranchMode::LoopContinue(l)
+                                | relooper::BranchMode::LoopContinueIntoMulti(l) => {
+                                    vec![Stmt::Continue(ContinueStmt {
+                                        span,
+                                        label: Some(Ident::new(
+                                            Atom::new(format!("${l}")),
+                                            span,
+                                            ctxt,
+                                        )),
+                                    })]
+                                }
+                                relooper::BranchMode::MergedBranch => vec![],
+                                relooper::BranchMode::MergedBranchIntoMulti => vec![],
+                                relooper::BranchMode::SetLabelAndBreak => {
+                                    vec![Stmt::Break(BreakStmt { span, label: None })]
+                                }
+                            },
+                        }
+                        .into_iter(),
+                    )
+                    .collect::<Vec<_>>()
+                };
+                let l = simple_block.label;
+                let body = self.blocks[l].stmts.iter().cloned().collect::<Vec<_>>();
+                let mut body = match &self.blocks[l].catch {
+                    Catch::Throw => body,
+                    Catch::Jump { pat, k } => {
+                        let id = Ident::new_private(Atom::new("caught"), span);
+                        vec![Stmt::Try(Box::new(TryStmt {
+                            span,
+                            block: BlockStmt {
+                                span,
+                                ctxt,
+                                stmts: body,
+                            },
+                            finalizer: None,
+                            handler: Some(CatchClause {
+                                span,
+                                param: Some(Pat::Ident(id.clone().into())),
+                                body: BlockStmt {
+                                    span,
+                                    ctxt,
+                                    stmts: vec![Stmt::Expr(ExprStmt {
+                                        span,
+                                        expr: Box::new(Expr::Assign(AssignExpr {
+                                            span,
+                                            op: swc_ecma_ast::AssignOp::Assign,
+                                            left: pat.clone().try_into().unwrap(),
+                                            right: Box::new(Expr::Ident(id)),
+                                        })),
+                                    })]
+                                    .into_iter()
+                                    .chain(jmp(*k).into_iter())
+                                    .collect(),
+                                },
+                            }),
+                        }))]
+                    }
+                };
+                match &self.blocks[l].term {
+                    Term::Return(expr) => body.push(Stmt::Return(ReturnStmt {
+                        span,
+                        arg: expr.as_ref().cloned().map(Box::new),
+                    })),
+                    Term::Throw(expr) => body.push(Stmt::Throw(ThrowStmt {
+                        span,
+                        arg: Box::new(expr.clone()),
+                    })),
+                    Term::Jmp(id) => body.extend(jmp(*id).into_iter()),
+                    Term::CondJmp {
+                        cond,
+                        if_true,
+                        if_false,
+                    } => body.push(Stmt::If(IfStmt {
+                        span,
+                        test: Box::new(cond.clone()),
+                        cons: Box::new(Stmt::Block(BlockStmt {
+                            span,
+                            ctxt,
+                            stmts: jmp(*if_true),
+                        })),
+                        alt: Some(Box::new(Stmt::Block(BlockStmt {
+                            span,
+                            ctxt,
+                            stmts: jmp(*if_false),
+                        }))),
+                    })),
+                    Term::Switch { x, blocks, default } => {
+                        body.push(Stmt::Switch(SwitchStmt {
+                            span,
+                            discriminant: Box::new(x.clone()),
+                            cases: blocks
+                                .iter()
+                                .map(|(i, k)| SwitchCase {
+                                    span,
+                                    test: Some(Box::new(i.clone())),
+                                    cons: jmp(*k),
+                                })
+                                .chain(once(SwitchCase {
+                                    span,
+                                    test: None,
+                                    cons: jmp(*default),
+                                }))
+                                .collect(),
+                        }));
+                    }
+                    Term::Default => {}
+                };
+                for x in [simple_block.immediate.as_ref(), simple_block.next.as_ref()] {
+                    body.extend(
+                        x.into_iter()
+                            .flat_map(|i| self.process_block(i.as_ref(), span, ctxt)),
+                    );
+                }
+                return body;
+            }
+            ShapedBlock::Loop(loop_block) => once(Stmt::Labeled(LabeledStmt {
+                span,
+                label: Ident::new(Atom::new(format!("${}", loop_block.loop_id)), span, ctxt),
+                body: Box::new(Stmt::While(WhileStmt {
+                    span: span,
+                    test: Box::new(Expr::Lit(Lit::Bool(Bool {
+                        span: span,
+                        value: true,
+                    }))),
+                    body: Box::new(Stmt::Block(BlockStmt {
+                        span,
+                        ctxt,
+                        stmts: self.process_block(&*loop_block.inner, span, ctxt),
+                    })),
+                })),
+            }))
+            .chain(
+                loop_block
+                    .next
+                    .as_ref()
+                    .into_iter()
+                    .flat_map(|a| self.process_block(&*a, span, ctxt).into_iter()),
+            )
+            .collect(),
+            ShapedBlock::Multiple(multiple_block) => vec![Stmt::Switch(SwitchStmt {
+                span,
+                discriminant: Box::new(Expr::Lit(Lit::Bool(Bool { span, value: true }))),
+                cases: multiple_block
+                    .handled
+                    .iter()
+                    .map(|h| SwitchCase {
+                        span,
+                        test: Some(Box::new(Expr::Call(CallExpr {
+                            span,
+                            ctxt,
+                            callee: swc_ecma_ast::Callee::Expr(Box::new(
+                                MemberExpr {
+                                    span,
+                                    obj: Box::new(Expr::Array(ArrayLit {
+                                        span,
+                                        elems: h
+                                            .labels
+                                            .iter()
+                                            .map(|l| {
+                                                Some(ExprOrSpread {
+                                                    spread: None,
+                                                    expr: Box::new(Expr::Lit(Lit::Str(Str {
+                                                        span,
+                                                        value: Atom::new(l.index().to_string()),
+                                                        raw: None,
+                                                    }))),
+                                                })
+                                            })
+                                            .collect(),
+                                    })),
+                                    prop: swc_ecma_ast::MemberProp::Ident(IdentName {
+                                        span,
+                                        sym: Atom::new("contains"),
+                                    }),
+                                }
+                                .into(),
+                            )),
+                            args: vec![ExprOrSpread {
+                                spread: None,
+                                expr: Box::new(Expr::Ident(Ident::new(
+                                    Atom::new("cff"),
+                                    span,
+                                    ctxt,
+                                ))),
+                            }],
+                            type_args: None,
+                        }))),
+                        cons: self
+                            .process_block(&h.inner, span, ctxt)
+                            .into_iter()
+                            .chain(
+                                if h.break_after {
+                                    Some(Stmt::Break(swc_ecma_ast::BreakStmt { span, label: None }))
+                                } else {
+                                    None
+                                }
+                                .into_iter(),
+                            )
+                            .collect(),
+                    })
+                    .collect(),
+            })],
+        }
+    }
 }
 pub struct Block {
     pub stmts: Vec<Stmt>,
     pub catch: Catch,
     pub term: Term,
 }
-#[derive(Clone,Default)]
+#[derive(Clone, Default)]
 pub enum Catch {
     #[default]
     Throw,
-    Jump { pat: Pat, k: Id<Block> },
+    Jump {
+        pat: Pat,
+        k: Id<Block>,
+    },
 }
 #[derive(Default)]
 pub enum Term {
@@ -46,7 +339,7 @@ pub struct Loop {
     pub r#break: Id<Block>,
     pub r#continue: Id<Block>,
 }
-#[derive(Clone,Default)]
+#[derive(Clone, Default)]
 pub struct Ctx {
     pub catch: Catch,
     pub cur_loop: Option<Loop>,

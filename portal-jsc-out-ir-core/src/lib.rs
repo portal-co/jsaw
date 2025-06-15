e1de7a1ba970379413306c76cd6338e859bdb222
Graham Kelly <gkgoat6700@gmail.com>
2025-06-15 03:23:29 +0000

    ll?

--- portal-jsc-out-ir-core/src/lib.rs
+++ portal-jsc-out-ir-core/src/lib.rs
@@ -4,3 +4,91 @@ pub mod llir;
 pub mod mobir;
 #[cfg(feature = "soup")]
 pub mod soup;
+
+use std::convert::Infallible;
+
+pub use swc_atoms::Atom;
+pub use swc_common::{Mark, Span, Spanned, SyntaxContext};
+macro_rules! withs {
+    ($([$a:ident => $b:ident])*) => {
+        $(
+        #[derive(Clone,Copy,Eq,PartialEq,Ord,PartialOrd,Hash,Debug)]
+        pub struct $b<T>{
+            pub wrap: T,
+            pub item: $a,
+        }
+        impl<T> $b<T>{
+            pub fn as_ref<'a>(&'a self) -> $b<&'a T>{
+                $b{
+                    item: self.item,
+                    wrap: &self.wrap,
+                }
+            }
+            pub fn as_mut<'a>(&'a mut self) -> $b<&'a mut T>{
+                $b{
+                    item: self.item,
+                    wrap: &mut self.wrap,
+                }
+            }
+            pub fn map<U,E>(self, f: impl FnOnce(T) -> Result<U,E>) -> Result<$b<U>,E>{
+                Ok($b{
+                    wrap: f(self.wrap)?,
+                    item: self.item,
+                })
+            }
+        }
+    )*
+    };
+}
+withs!([Span => WithSpan] [SyntaxContext => Contextual]);
+impl<T: Default> Default for WithSpan<T>{
+    fn default() -> Self {
+        Self { wrap: Default::default(), item: Span::dummy_with_cmt() }
+    }
+}
+impl<T: Default> Default for Contextual<T>{
+    fn default() -> Self {
+        Self { wrap: Default::default(), item: Default::default() }
+    }
+}
+#[derive(Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash, Debug,Default)]
+pub struct ContextuallyWithSpan<T>(pub WithSpan<Contextual<T>>);
+impl<T> ContextuallyWithSpan<T> {
+    pub fn as_ref<'a>(&'a self) -> ContextuallyWithSpan<&'a T> {
+        ContextuallyWithSpan(
+            self.0
+                .as_ref()
+                .map(|a| Ok::<_, Infallible>(a.as_ref()))
+                .unwrap(),
+        )
+    }
+    pub fn as_mut<'a>(&'a mut self) -> ContextuallyWithSpan<&'a mut T> {
+        ContextuallyWithSpan(
+            self.0
+                .as_mut()
+                .map(|a| Ok::<_, Infallible>(a.as_mut()))
+                .unwrap(),
+        )
+    }
+    pub fn map<U, E>(
+        self,
+        f: impl FnOnce(T) -> Result<U, E>,
+    ) -> Result<ContextuallyWithSpan<U>, E> {
+        self.0.map(move |a| a.map(f)).map(ContextuallyWithSpan)
+    }
+}
+impl<T> Spanned for WithSpan<T> {
+    fn span(&self) -> Span {
+        self.item
+    }
+}
+impl<T: Spanned> Spanned for Contextual<T> {
+    fn span(&self) -> Span {
+        self.wrap.span()
+    }
+}
+impl<T> Spanned for ContextuallyWithSpan<T> {
+    fn span(&self) -> Span {
+        self.0.span()
+    }
+}
---
+++ portal-jsc-out-ir-core/src/llir.rs
@@ -0,0 +1,104 @@
+use id_arena::{Arena, Id};
+use swc_common::Span;
+
+use crate::WithSpan;
+
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Debug)]
+pub enum Size {
+    Bits { bits: usize },
+    Ptr,
+}
+#[derive(Clone, Default)]
+pub struct LLCfg {
+    pub blocks: Arena<LLBlock>,
+    pub values: Arena<LLValue>,
+}
+impl LLCfg {
+    pub fn add_blockparam(&mut self, k: Id<LLBlock>, ty: Size) -> Id<LLValue> {
+        let v = self.values.alloc(crate::WithSpan {
+            wrap: LLValueInternal::Param {
+                block: k,
+                size: ty,
+                index: self.blocks[k].params.len(),
+            },
+            item: Span::dummy_with_cmt(),
+        });
+        self.blocks[k].params.push((v, ty));
+        return v;
+    }
+    pub fn append_to_block(&mut self, k: Id<LLBlock>, a: LLValue) -> Id<LLValue> {
+        let val = self.values.alloc(a);
+        self.blocks[k].stmts.push(val);
+        val
+    }
+}
+#[derive(Clone)]
+pub struct LLFunc {
+    pub cfg: LLCfg,
+    pub root: Id<LLBlock>,
+}
+impl Default for LLFunc {
+    fn default() -> Self {
+        let mut cfg = LLCfg::default();
+        let root = cfg.blocks.alloc(Default::default());
+        Self { cfg, root }
+    }
+}
+#[derive(Clone, Default)]
+pub struct LLBlock {
+    pub params: Vec<(Id<LLValue>, Size)>,
+    pub stmts: Vec<Id<LLValue>>,
+    pub term: WithSpan<LLTerm>,
+}
+pub type LLValue = WithSpan<LLValueInternal>;
+#[derive(Clone)]
+pub enum LLValueInternal {
+    ArithOp {
+        size: Size,
+        lhs: Id<LLValue>,
+        op: portal_pc_asm_common::types::Arith,
+        rhs: Id<LLValue>,
+    },
+    CmpOp {
+        size: Size,
+        lhs: Id<LLValue>,
+        op: portal_pc_asm_common::types::Cmp,
+        rhs: Id<LLValue>,
+    },
+    Load {
+        size: Size,
+        val: Id<LLValue>,
+    },
+    Store {
+        size: Size,
+        ptr: Id<LLValue>,
+        val: Id<LLValue>,
+    },
+    ExtOrTrunc {
+        extension: portal_pc_asm_common::types::Ext,
+        val: Id<LLValue>,
+        target: Size,
+    },
+    Param {
+        block: Id<LLBlock>,
+        size: Size,
+        index: usize,
+    },
+}
+#[derive(Clone)]
+pub struct LLTarget {
+    pub block: Id<LLBlock>,
+    pub args: Vec<Id<LLValue>>,
+}
+#[derive(Clone, Default)]
+pub enum LLTerm {
+    Ret(Id<LLValue>),
+    Jmp(LLTarget),
+    CondJmp {
+        val: Id<LLValue>,
+        then: LLTarget,
+        otherwise: LLTarget,
+    },
+    #[default]
+    Default,
+}

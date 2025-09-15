2c0cdd1c5acd578de3484a646d513b877560558b
Graham Kelly <gkgoat6700@gmail.com>
2025-09-15 00:25:21 +0000

    core 2

--- js-runtime-lib/src/synth.rs
+++
@@ -1,9 +0,0 @@
-pub fn throw_type_error<T>() -> T{
-    panic!("type error")
-}
-pub fn throw_runtime_error<T>() -> T{
-    panic!("runtime error")
-}
-pub fn throw_reference_error<T>() -> T{
-    panic!("reference error")
-}

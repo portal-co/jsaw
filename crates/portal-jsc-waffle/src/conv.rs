b597bd5f8764de3bd39e10232834373b5247d453
gkgoat <65402989+gkgoat1@users.noreply.github.com>
2026-04-30 13:18:47 -0400

    starting

--- crates/portal-jsc-waffle/src/conv.rs
+++ crates/portal-jsc-waffle/src/conv.rs
@@ -3,8 +3,9 @@ use std::{
     mem::take,
 };
 
-use portal_jsc_swc_ssa::SFunc;
-use portal_pc_waffle::{Module, Type, WithNullable};
+use portal_jsc_swc_ssa::{SFunc, SValue};
+use portal_jsc_swc_tac::{Item, TTerm};
+use portal_pc_waffle::{Module, Operator, Type, Value, WithNullable};
 
 pub fn convert<'a>(root: &'a SFunc, module: &mut Module) {
     let object = module
@@ -63,27 +64,42 @@ pub fn convert<'a>(root: &'a SFunc, module: &mut Module) {
             }
         };
     }
-    while let Some((sfunc, sblock)) = workqueue.pop_front() {
+    macro_rules! bcache {
+        ($func:expr, $cache:expr, $sblock:expr, $sfunc:expr) => {
+            match $func {
+                func => match $cache {
+                    cache => match $sblock {
+                        sblock => match $sfunc {
+                            sfunc => *cache.entry(sblock).or_insert_with(|| {
+                                let func = module
+                                    .funcs
+                                    .get_mut(func)
+                                    .and_then(|a| a.body_mut())
+                                    .unwrap();
+                                let b = func.add_block();
+                                for p in &sfunc.cfg.blocks[sblock].params {
+                                    func.add_blockparam(
+                                        b,
+                                        Type::Heap(WithNullable {
+                                            nullable: true,
+                                            value: portal_pc_waffle::HeapType::Sig {
+                                                sig_index: object,
+                                            },
+                                        }),
+                                    );
+                                }
+                                b
+                            }),
+                        },
+                    },
+                },
+            }
+        };
+    }
+    while let Some((sfunc, mut sblock)) = workqueue.pop_front() {
         let (func, cache) = fcache!(sfunc);
         let func = *func;
-        let mut block = *cache.entry(sblock).or_insert_with(|| {
-            let func = module
-                .funcs
-                .get_mut(func)
-                .and_then(|a| a.body_mut())
-                .unwrap();
-            let b = func.add_block();
-            for p in &sfunc.cfg.blocks[sblock].params {
-                func.add_blockparam(
-                    b,
-                    Type::Heap(WithNullable {
-                        nullable: true,
-                        value: portal_pc_waffle::HeapType::Sig { sig_index: object },
-                    }),
-                );
-            }
-            b
-        });
+        let mut block = bcache!(func, cache, sblock, &sfunc);
         let mut vals = sfunc.cfg.blocks[sblock]
             .params
             .iter()
@@ -96,20 +112,70 @@ pub fn convert<'a>(root: &'a SFunc, module: &mut Module) {
             )
             .collect::<BTreeMap<_, _>>();
         let mut blkset = [(block, vals)].into_iter().collect::<BTreeMap<_, _>>();
-        for stmt in sfunc.cfg.blocks[sblock].stmts.iter().cloned() {
-            for (mut block, mut vals) in take(&mut blkset) {
-                let val: portal_pc_waffle::Value = match &sfunc.cfg.values[stmt].value {
-                    _ => todo!("unsupported statement: {:?}", sfunc.cfg.values[stmt].value),
-                };
-                vals.insert(stmt, val);
-                blkset.insert(block, vals);
+        loop {
+            for stmt in sfunc.cfg.blocks[sblock].stmts.iter().cloned() {
+                for (mut block, mut vals) in take(&mut blkset) {
+                    let val: portal_pc_waffle::Value = match &sfunc.cfg.values[stmt].value {
+                        SValue::Item { item, span } => match item {
+                            Item::Just { id } => vals.get(id).cloned().unwrap_or_else(|| {
+                                panic!("undefined value: {:?} at {:?}", id, span)
+                            }),
+                            _ => todo!("unsupported item: {:?}", item),
+                        },
+                        _ => todo!("unsupported statement: {:?}", sfunc.cfg.values[stmt].value),
+                    };
+                    vals.insert(stmt, val);
+                    blkset.insert(block, vals);
+                }
             }
-        }
-        for (block,vals) in blkset{
-            let terminator = match &sfunc.cfg.blocks[sblock].postcedent.term{
-                _ => todo!("unsupported terminator")
-            };
-            module.funcs[func].body_mut().unwrap().set_terminator(block, terminator);
+            let mut needs_break = true;
+            for (block, vals) in take(&mut blkset) {
+                let terminator = match &sfunc.cfg.blocks[sblock].postcedent.term {
+                    TTerm::Jmp(t) => {
+                        needs_break = false;
+                        sblock = t.block;
+                        blkset.insert(
+                            block,
+                            sfunc.cfg.blocks[sblock]
+                                .params
+                                .iter()
+                                .map(|a| a.0)
+                                .zip(t.args.iter().filter_map(|a| vals.get(a)).cloned())
+                                .collect::<BTreeMap<_, _>>(),
+                        );
+                        continue;
+                    }
+                    TTerm::Return(r) => match r {
+                        Some(r) => portal_pc_waffle::Terminator::Return {
+                            values: vec![vals.get(r).cloned().unwrap_or_else(|| {
+                                panic!("undefined return value: {:?} at block {:?}", r, block)
+                            })],
+                        },
+                        None => {
+                            let ty = Type::Heap(WithNullable {
+                                nullable: true,
+                                value: portal_pc_waffle::HeapType::Sig { sig_index: object },
+                            });
+                            portal_pc_waffle::Terminator::Return {
+                                values: vec![module.funcs[func].body_mut().unwrap().add_op(
+                                    block,
+                                    Operator::RefNull { ty: ty.clone() },
+                                    &[],
+                                    &[ty],
+                                )],
+                            }
+                        }
+                    },
+                    _ => todo!("unsupported terminator"),
+                };
+                module.funcs[func]
+                    .body_mut()
+                    .unwrap()
+                    .set_terminator(block, terminator);
+            }
+            if needs_break {
+                break;
+            }
         }
     }
 }
